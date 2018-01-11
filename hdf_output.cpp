b7aa24fbfda617b46aac2e0588c8813f653ad52f
Heeho Park <hdpark2@illinois.edu>
2018-01-10 22:36:28 -0600

    hdf output modified to match PFLOTRAN output.

--- hdf_output.cpp
+++ hdf_output.cpp
@@ -11,17 +11,17 @@ using namespace std;
 
 void output_h5(int nx, int ny, gsl_matrix* sol, double time) {
   hid_t fid, gid, dsetid, dspaid;
-  hsize_t dims[2];
+  hsize_t dims[3];
   herr_t status;
-  double ans[nx][ny];
+  double ans[nx][ny][1];
   const char* time_ptr;
   const char* dset_ptr;
 
   stringstream time_stream;
-  time_stream << fixed << setprecision(5) << time;
+  time_stream << scientific << setprecision(5) << time;
   string time_string = time_stream.str();
-  string title = "Time: ";
-  string unit = " [sec]";
+  string title = "Time:  ";
+  string unit = " s";
   string group_name;
   string dataset_name = "u";
 
@@ -34,17 +34,18 @@ void output_h5(int nx, int ny, gsl_matrix* sol, double time) {
 
   dims[0] = nx;
   dims[1] = ny;
+  dims[2] = 1;
 
   fid = H5Fopen(FILE, H5F_ACC_RDWR, H5P_DEFAULT);
   gid = H5Gcreate(fid, time_ptr, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
   
-  dspaid = H5Screate_simple(2, dims, NULL);  
+  dspaid = H5Screate_simple(3, dims, NULL);  
   dsetid = H5Dcreate(gid, dset_ptr, H5T_NATIVE_DOUBLE, dspaid, \
 		       H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
   for (int i = 0; i < nx; i++) {
     for (int j = 0; j < ny; j++) {
-      ans[i][j] = gsl_matrix_get(sol,i,j);
+      ans[i][j][0] = gsl_matrix_get(sol,i,j);
     }
   }
   status = H5Dwrite(dsetid, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, \
@@ -56,23 +57,28 @@ void output_h5(int nx, int ny, gsl_matrix* sol, double time) {
 }
 
 void init_h5(double *& x_lin, double *& y_lin, int nx, int ny) {
-  hid_t fid, gid, dsetid_x, dsetid_y, dspaceid_x, dspaceid_y;
-  hsize_t x_dims[1], y_dims[1];
+  hid_t fid, gid, dsetid_x, dsetid_y, dsetid_z, dspaceid_x, dspaceid_y, dspaceid_z;
+  hsize_t x_dims[1], y_dims[1], z_dims[1];
   herr_t status;
   double x[nx];
   double y[ny];
+  double z[2];
 
   x_dims[0] = nx;
   y_dims[0] = ny;
+  z_dims[0] = 2;
 
   fid = H5Fcreate(FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
   gid = H5Gcreate(fid, "Coordinates", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
   
   dspaceid_x = H5Screate_simple(1, x_dims, NULL);
   dspaceid_y = H5Screate_simple(1, y_dims, NULL);
-  dsetid_x = H5Dcreate(fid, "Coordinates/X", H5T_NATIVE_DOUBLE, dspaceid_x, \
+  dspaceid_z = H5Screate_simple(1, z_dims, NULL);
+  dsetid_x = H5Dcreate(gid, "X [m]", H5T_NATIVE_DOUBLE, dspaceid_x, \
 		       H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  dsetid_y = H5Dcreate(fid, "Coordinates/Y", H5T_NATIVE_DOUBLE, dspaceid_y, \
+  dsetid_y = H5Dcreate(gid, "Y [m]", H5T_NATIVE_DOUBLE, dspaceid_y, \
+		       H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  dsetid_z = H5Dcreate(gid, "Z [m]", H5T_NATIVE_DOUBLE, dspaceid_z, \
 		       H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
   for (int i = 0; i < nx; i++) {
@@ -81,15 +87,22 @@ void init_h5(double *& x_lin, double *& y_lin, int nx, int ny) {
   for (int i = 0; i < ny; i++) {
     y[i] = y_lin[i];
   }
+  for (int i = 0; i < 2; i++) {
+    z[i] = double(i);
+  }
 
   status = H5Dwrite(dsetid_x, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, \
 		    H5P_DEFAULT, x);
   status = H5Dwrite(dsetid_y, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, \
 		    H5P_DEFAULT, y);
+  status = H5Dwrite(dsetid_z, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, \
+		    H5P_DEFAULT, z);
   status = H5Dclose(dsetid_x);
   status = H5Sclose(dspaceid_x);
   status = H5Dclose(dsetid_y);
   status = H5Sclose(dspaceid_y);
+  status = H5Dclose(dsetid_z);
+  status = H5Sclose(dspaceid_z);
   status = H5Gclose(gid);
   status = H5Fclose(fid);
 
